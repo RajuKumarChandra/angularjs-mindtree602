7f888ace5179216c396ca80c33744a8fbbc2b395
curanest <curanest@gmail.com>
2016-09-23 16:04:59 +0530

    directive

---
+++ Day8/directives.js
@@ -0,0 +1,17 @@
+angular.module("customDirectives",[])
+.directive("triButton", function(){
+	return {
+		scope: {counter: "=counter"},
+		link: function(scope,element,attrs){
+			element.on("click", function(event){
+				console.log("Button click " + event.target.innerText);
+				scope.$apply(function(){
+					scope.counter++;
+					console.log("counter " + scope.counter);
+				})
+			})
+		}
+
+	}
+
+});
